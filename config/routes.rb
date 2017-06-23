78e928c14ffea12a38dbbe2a81e4b88fbb3dbd66
Jane Wang <jw9billions@gmail.com>
2017-06-22 23:05:40 -0700

    cp18

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  resources :posts
 
-  get 'welcome/about'
+  get 'about' => 'welcome#about'
 
-  get 'welcome/contact'
+  #get 'welcome/contact'
 
   root 'welcome#index'
 
