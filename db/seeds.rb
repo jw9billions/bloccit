9ee79cb58edbbe32e12f742a77a9f4b8aecba4ad
Jane Wang <jw9billions@gmail.com>
2017-07-17 18:00:28 -0700

    cp28

--- db/seeds.rb
+++ db/seeds.rb
@@ -42,11 +42,18 @@ posts = Post.all
 end
 comments = Comment.all
 
-user = User.first
-user.update_attributes!(
-  email: 'jw9billions@gmail.com',
-  password: 'helloworld'
-)
+admin = User.create!(
+   name:     'Admin User',
+   email:    'admin@example.com',
+   password: 'helloworld',
+   role:     'admin'
+ )
+
+ member = User.create!(
+   name:     'Member User',
+   email:    'member@example.com',
+   password: 'helloworld'
+ )
 
 puts "Seed finished"
 puts "#{User.count} users created"
