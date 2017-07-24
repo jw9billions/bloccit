16227e244deaccf24e506bd3842e521e392de3e9
Jane Wang <jw9billions@gmail.com>
2017-07-23 21:28:10 -0700

    cp31

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,7 +36,10 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
+  config.raise_delivery_errors = true
+  
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   config.action_mailer.default_url_options = { host: 'localhost' }
+
 end
